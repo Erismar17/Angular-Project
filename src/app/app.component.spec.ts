3ce8bcfc6d23074d898acfbc4f8bd6ac27aa5bdd
Erismar <125566354+Erismar17@users.noreply.github.com>
2024-04-29 12:14:04 -0500

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+/* import { PokemonsFormComponent } from './app/pokemons-form' */
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent,
+/*         PokemonsFormComponent, */
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +22,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-app');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-app');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('PokedexV15 app is running!');
   });
 });
---
+++ src/app/pokemons-form/pokemons-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pokemons-form',
+  templateUrl: './pokemons-form.component.html',
+  styleUrls: ['./pokemons-form.component.css']
+})
+export class PokemonsFormComponent {
+
+}
