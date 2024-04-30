3521a198d159f076159b5c679667dd1f7442cfde
Erismar <125566354+Erismar17@users.noreply.github.com>
2024-04-30 16:26:20 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonsDetailsComponent } from './pokemons/pokemons-details/pokemons-details.component';
+import { CardComponent } from './pokemons/card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'list-pokemons',
+    pathMatch: 'full',
+  },
+  {
+    path: 'list-pokemons',
+    component: CardComponent
+  },
+  {
+    path: 'detail-pokemon',
+    component: PokemonsDetailsComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'list-pokemons',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
